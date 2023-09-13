d686338f5d69c49a4a3d6bed399e57a19031cbc8
pixcelo <bodomu@gmail.com>
2023-09-14 03:20:22 +0900

    update

---
+++ trade/main.py
@@ -0,0 +1,60 @@
+import MetaTrader5 as mt5
+import time
+import pandas as pd
+import traceback
+from trading import Trading
+
+def main_process(symbol, timeframe, lot_size=10000, polling_interval=60):
+    trading = Trading(symbol)
+    # MT5に接続
+    if not mt5.initialize():
+        print("initialize() failed, error code =", mt5.last_error())
+        quit()
+
+    # ポートフォリオを初期化
+    portfolio = {'position': None, 'entry_price': None}
+
+    try:
+        while True:
+            rates = mt5.copy_rates_from_pos(symbol, timeframe, 0, 200)  # 直近200データを取得
+            if rates is None:
+                print("Error in copy_rates_from_pos(), error code =", mt5.last_error())
+                continue
+
+            df = pd.DataFrame(rates)
+            df['time'] = pd.to_datetime(df['time'], unit='s')
+            df = trading.load_data(df)
+
+            position = trading.get_position()
+
+            lot = 0.1 # 1ロット=100,000通貨　(最小取引数量 10,000通貨)
+            signal = trading.trade_conditions(symbol, df, len(df)-1, portfolio, lot)
+
+            # order parameters
+            order_type = mt5.ORDER_TYPE_BUY
+            point = mt5.symbol_info(symbol).point
+            price = mt5.symbol_info_tick(symbol).ask
+            stop_loss = price - 100 * point
+            take_profit = price + 100 * point
+            #trading.place_order(symbol, order_type, lot, price, stop_loss, take_profit)
+            if signal == 'entry_long' and position is None:
+                order_type = mt5.ORDER_TYPE_BUY
+                trading.place_order(symbol, order_type, lot, price, stop_loss, take_profit)
+                portfolio['position'] = 'long'
+            elif signal == 'exit_long' and portfolio['position'] == 'long':
+                order_type = mt5.ORDER_TYPE_SELL
+                trading.close_position(position["id"])
+                portfolio['position'] = None
+                portfolio['entry_price'] = None
+
+            time.sleep(polling_interval)
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+        traceback.print_exc()
+    finally:
+        # MT5との接続を閉じる
+        mt5.shutdown()
+
+
+main_process('EURUSD', mt5.TIMEFRAME_M1)
---
+++ trade/trading.py
@@ -0,0 +1,140 @@
+import sys
+sys.path.append('d:\\dev\\mt5-python')
+
+from modules import ResampleData, TradingStrategy
+import MetaTrader5 as mt5
+import numpy as np
+
+class Trading:
+    def __init__(self, symbol, lot_size=1, slippage=3):
+        self.symbol = symbol
+        self.lot_size = lot_size
+        self.slippage = slippage
+        self.strategy = TradingStrategy(allow_short=True)
+
+        self.portfolio = {
+            'position': None,  # 'long' or 'short'
+            'entry_price': None,
+            'take_profit': None,
+            'stop_loss': None,
+            'profit': 0
+        }
+    
+    def load_data(self, df):
+        resampler_df = ResampleData(df)
+        df = resampler_df.merge_data()
+        df = self.strategy.prepare_data(df)
+        return df
+
+    def place_order(self, symbol, order_type, volume, price, stop_loss, take_profit):
+        try:
+            # 注文プロパティをセット
+            request = {
+                "action": mt5.TRADE_ACTION_DEAL,
+                "symbol": symbol,
+                "volume": volume,
+                "type": order_type,
+                "price": price,
+                "sl": stop_loss,
+                "tp": take_profit,
+                "deviation": self.slippage, # point単位
+                "magic": 234000,
+                "comment": "python script open",
+                "type_time": mt5.ORDER_TIME_GTC,
+                "type_filling": mt5.ORDER_FILLING_IOC,
+            }
+
+            # 注文を実行
+            result = mt5.order_send(request)
+            if result.retcode != mt5.TRADE_RETCODE_DONE:
+                raise Exception("order_send failed, retcode={}".format(result.retcode))
+
+            print("Order placed successfully!")
+            return result
+
+        except Exception as e:
+            print("An error occurred while placing order:", str(e))
+            return None
+
+    def cancel_order(self, order_ticket):
+        try:
+            order = mt5.order_get(ticket=order_ticket)
+            if order == None:
+                raise Exception("Order not found")
+
+            request = {
+                "action": mt5.TRADE_ACTION_REMOVE,
+                "order": order_ticket,
+            }
+            result = mt5.order_send(request)
+            if result.retcode != mt5.TRADE_RETCODE_DONE:
+                raise Exception("Failed to cancel order, retcode={}".format(result.retcode))
+
+            print("Order canceled successfully!")
+            return result
+
+        except Exception as e:
+            print("An error occurred while canceling order:", str(e))
+            return None
+
+    def close_position(self, position_ticket):
+        try:
+            position = mt5.positions_get(ticket=position_ticket)
+            if position == None:
+                raise Exception("Position not found")
+            
+            pos = position[0]
+
+            request = {
+                "action": mt5.TRADE_ACTION_DEAL,
+                "symbol": pos.symbol,
+                "volume": pos.volume,
+                "type": mt5.ORDER_TYPE_SELL if pos.type == mt5.ORDER_TYPE_BUY else mt5.ORDER_TYPE_BUY,
+                "position": pos.ticket,
+                "price": mt5.symbol_info_tick(pos.symbol).ask if pos.type == mt5.ORDER_TYPE_BUY else mt5.symbol_info_tick(pos.symbol).bid,
+                "deviation": self.slippage,
+                "magic": 234001,
+                "comment": "python script close",
+                "type_time": mt5.ORDER_TIME_GTC,
+                "type_filling": mt5.ORDER_FILLING_IOC,
+            }
+            result = mt5.order_send(request)
+            if result.retcode != mt5.TRADE_RETCODE_DONE:
+                raise Exception("Failed to close position, retcode={}".format(result.retcode))
+
+            print("Position closed successfully!")
+            return result
+
+        except Exception as e:
+            print("An error occurred while closing position:", str(e))
+        return None
+
+
+    def get_position(self):
+        # MT5のすべての開いているポジションを取得
+        positions = mt5.positions_get()
+
+        if positions == None:
+            print('No positions found, error code:', mt5.last_error())
+            return None
+
+        # 特定の通貨ペアに対応するポジションを探す
+        for position in positions:
+            if position.symbol == self.symbol:
+                # ポジションの詳細を辞書として返す
+                return {
+                    'id': position.ticket,
+                    'symbol': position.symbol,
+                    'type': position.type,
+                    'volume': position.volume,
+                    'price_open': position.price_open,
+                    'sl': position.sl,
+                    'tp': position.tp,
+                    'profit': position.profit,
+                    # 他の必要な属性...
+                }
+
+        return None
+    
+    def trade_conditions(self, symbol, df, i, portfolio, lot_size):
+        return self.strategy.trade_conditions_func(symbol, df, i, portfolio, lot_size)
